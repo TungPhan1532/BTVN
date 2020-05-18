add15392faa2424fd4a47353955075c99fff7c11
TungPhan1532 <65560399+TungPhan1532@users.noreply.github.com>
2020-05-19 00:42:52 +0700

    Add files via upload

---
+++ BTVN_Tuan1/BTVN_Tuan1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BTVN_Tuan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnplay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnplay
+            // 
+            this.btnplay.Location = new System.Drawing.Point(379, 91);
+            this.btnplay.Name = "btnplay";
+            this.btnplay.Size = new System.Drawing.Size(80, 43);
+            this.btnplay.TabIndex = 0;
+            this.btnplay.Text = "Play";
+            this.btnplay.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(349, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GAME CARO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 415);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnplay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BTVN_Tuan1/BTVN_Tuan1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN_Tuan1
+{
+    public partial class Form1 : Form
+    {
+       
+        Button b;
+        //int x=0; int o=1;
+        public Form1()
+        {
+            InitializeComponent();
+            banco(10);
+        }
+
+        public void banco(int n)
+        {
+            for (int i = 0; i < n * 20; i += 20)
+            {
+                for (int j = 0; j < n * 20; j += 20)
+                {
+                    demo1(i, j);
+                }
+            }
+        }
+        public void demo1(int top, int left)
+        {
+
+
+            //but1
+            Button b = new Button();
+            b.Name = "b1";
+            b.Text = "";
+            b.Size = new Size(30, 30);
+            b.Top = top;
+            b.Left = left;
+
+            this.Controls.Add(b);
+
+            b.Click += new System.EventHandler(bt_Click);
+        }
+
+
+
+        
+        private void bt_Click(object sender, EventArgs e)
+        {
+            //if (x == 0)
+            //    x = 1;
+            //else
+            //    x = 0;
+            
+            
+            Button b = sender as Button;
+            //if(x==1)
+            b.BackgroundImage = Image.FromFile(Application.StartupPath + "\\icon\\x.png");
+            //else
+                b.BackgroundImage = Image.FromFile(Application.StartupPath + "\\icon\\o.png");
+             
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
